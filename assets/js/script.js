6ec115f668034fd1b9ce49eadb00577e5aefd661
jmg <macgreene14@gmail.com>
2022-08-15 14:28:28 -0700

    recent city search included:

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 var api_key = "9a890473fa5b28d71ed016387dd3738c"
 var currentWeatherEl = document.getElementById("currentWeather")  // container for injecting current weather elements
 var forecastWeatherEl = document.getElementById("forecastWeather")  // container for injecting forecast weather elements
+var searchBarEl = document.getElementById("searchBar")  // container for injecting search history
+
 var searchBtn = document.getElementById("search")  // button for exe search
 
 
@@ -9,7 +11,21 @@ var storedCities = JSON.parse(localStorage.getItem("weather-app"))
 if (storedCities) {
     // if available, loop through object and display each city
     // add data attribute with city name to reference within function when clicked
-    console.log(storedCities)
+    var storedCitiesVal = Object.values(storedCities)
+    for (var i = 0; i < storedCitiesVal.length; i++ ) {
+
+        newEl = document.createElement("p")
+        var storedCity = storedCitiesVal[i]
+        newEl.textContent = (storedCity.charAt(0).toUpperCase() + storedCity.slice(1))
+        newEl.addEventListener("click", function(event) {
+            get_weather(event.target.innerText)
+        })
+        
+        searchBarEl.appendChild(newEl)
+    }
+
+} else {
+    console.log('no local data')
 }
 
 
@@ -17,7 +33,6 @@ function get_weather(city) {
     // get city name from coordinates
     var url_city = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${api_key}`
     console.log("URL", url_city)
-    console.log(city)
 
     fetch(url_city)
         .then(function(response) {
@@ -43,7 +58,6 @@ function get_weather(city) {
             .then(function(data) {
                 console.log(data);
                 // pull current weather, add to html
-                console.log(data.current)
                 var dt_unix_0 = data.current.dt
                 var dt_0 = dayjs.unix(dt_unix_0).format("MM-DD-YYYY")
 
@@ -52,12 +66,20 @@ function get_weather(city) {
                 var humidity_0 = data.current.humidity
                 var uv_0 = data.current.uvi
 
+                var icon_id = data.current.weather[0].icon
+                var icon_url = `http://openweathermap.org/img/wn/${icon_id}@2x.png`
+
                 // clear previous content
                 currentWeatherEl.textContent = ""
 
                 // create card for current weather
                 card = document.createElement("div")
                 card.setAttribute("class", "card")
+
+                cardImgDiv = document.createElement("div")
+                cardImgDiv.setAttribute("class", "card-image")
+                cardImg = document.createElement("img")
+                cardImg.setAttribute("src", icon_url)
 
                 cardContent = document.createElement("div")
                 cardContent.setAttribute("class", "card-content")
@@ -66,6 +88,7 @@ function get_weather(city) {
                 content.setAttribute("class", "content")
 
                 cardContent.appendChild(content)
+                card.appendChild(cardImg)
                 card.appendChild(cardContent)
 
                 newEl = document.createElement("h3")
@@ -81,32 +104,49 @@ function get_weather(city) {
                 content.appendChild(newEl)
     
                 newEl = document.createElement("p")
-                newEl.textContent = ("Temperature: " + temp_0)
+                newEl.textContent = ("Temperature [°F]: " + temp_0)
                 content.appendChild(newEl)
     
                 newEl = document.createElement("p")
-                newEl.textContent = ("Wind Speed : " + wind_0)
+                newEl.textContent = ("Wind Speed [mph]: " + wind_0)
                 content.appendChild(newEl)
     
                 newEl = document.createElement("p")
-                newEl.textContent = ("Humidity: " + humidity_0)
+                newEl.textContent = ("Humidity [%]: " + humidity_0)
                 content.appendChild(newEl)
 
     
                 newEl = document.createElement("p")
                 newEl.textContent = ("UV Index: " + uv_0)
+
                 content.appendChild(newEl)
+
+                // color code uvi based on value
+
+                if (uv_0 > 7) {
+                    //red
+                    newEl.style.backgroundColor = "red"
+                } else if (uv_0 > 5) {
+                    // orange
+                    newEl.style.backgroundColor = "orange"
+                } else if (uv_0 > 2) {
+                    // yellow
+                    newEl.style.backgroundColor = "yellow"
+                } else {
+                    //green
+                    newEl.style.backgroundColor = "green"
+                }
 
                 // add card to container
                 currentWeatherEl.appendChild(card)
 
                 // set title for 5 day forecast
+                forecastWeatherEl.textContent = ""
                 newEl = document.createElement("h3")
                 newEl.textContent = ("5 day forecast:")
                 forecastWeatherEl.appendChild(newEl)
 
                 for (var i = 1; i < 6; i ++) {
-                    console.log(data.daily[i])
                     var dt_unix_1 = data.daily[i].dt
                     var dt_1 = dayjs.unix(dt_unix_1).format("MM-DD-YYYY")
 
@@ -114,10 +154,17 @@ function get_weather(city) {
                     var wind_1 = data.daily[i].wind_speed
                     var humidity_1 = data.daily[i].humidity
             
-                    
+                    var icon_id = data.daily[i].weather[0].icon
+                    var icon_url = `http://openweathermap.org/img/wn/${icon_id}@2x.png`
+
                     // create card
                     card = document.createElement("div")
                     card.setAttribute("class", "card square")
+
+                    cardImgDiv = document.createElement("div")
+                    cardImgDiv.setAttribute("class", "card-image")
+                    cardImg = document.createElement("img")
+                    cardImg.setAttribute("src", icon_url)
 
                     cardContent = document.createElement("div")
                     cardContent.setAttribute("class", "card-content")
@@ -126,6 +173,7 @@ function get_weather(city) {
                     content.setAttribute("class", "content")
 
                     cardContent.appendChild(content)
+                    card.appendChild(cardImg)
                     card.appendChild(cardContent)
 
                     // set content
@@ -135,18 +183,17 @@ function get_weather(city) {
                     content.appendChild(newEl)
 
                     newEl = document.createElement("p")
-                    newEl.textContent = ("Temperature: " + temp_1)
+                    newEl.textContent = ("Temperature [°F]: " + temp_1)
                     content.appendChild(newEl)
         
                     newEl = document.createElement("p")
-                    newEl.textContent = ("Wind Speed : " + wind_1)
+                    newEl.textContent = ("Wind Speed [mph]: " + wind_1)
                     content.appendChild(newEl)
 
                     newEl = document.createElement("p")
-                    newEl.textContent = ("Humidity : " + humidity_1)
+                    newEl.textContent = ("Humidity [%]: " + humidity_1)
                     content.appendChild(newEl)
-    
-                    
+
                     forecastWeatherEl.appendChild(card)
                 }
 
@@ -155,13 +202,35 @@ function get_weather(city) {
                 // does storedCities need to be called, or can reference outter scope?
                 var storedCities = JSON.parse(localStorage.getItem("weather-app"))
                 if (storedCities) {
-                    storedCities.append(city) // save new city to local storage, limit to 5
-                    // if available, loop through object and display each city
-                    // add data attribute with city name to reference within function when clicked
-                    console.log(storeCities)
+                    console.log(storedCities)
+                    var storeCities = {
+                        "1": city,
+                        "2": storedCities["1"],
+                        "3": storedCities["2"],
+                        "4": storedCities["3"],
+                        "5": storedCities["4"], 
+                    }
+
+                    localStorage.setItem("weather-app", JSON.stringify(storeCities));
+
+
+                } else {
+                    // create object for first time 
+                    var storeCities = {
+                        "1": city,
+                        "2": "",
+                        "3": "",
+                        "4": "",
+                        "5": "",
+
+                    };
+
+                    localStorage.setItem("weather-app", JSON.stringify(storeCities));
                 }
+
+
     
-                return data;
+                return;
             })
         })    
 }
